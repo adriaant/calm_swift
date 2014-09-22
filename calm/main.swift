54daf4e48bba5c9a8774b0ead8f65a23f31c1b97
Adriaan Tijsseling <adriaan@tijsseling.com>
2014-09-22 21:37:32 +0200

    Initial Commit

---
+++ calm/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  calm
+//
+//  Created by Adriaan Tijsseling on 9/22/14.
+//  Copyright (c) 2014 infinite-sushi.com. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
